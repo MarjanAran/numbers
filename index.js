10f4db174fb9898d40c93ea40e0b357bbffbb3db
MarjanAran <whc.rion@gmail.com>
2016-10-06 14:21:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-var result, i;
-var sum1 = 0, sum2 = 1; // 0 and 1
-    if (n == 0) return sum1;    //result for 0
-    if (n == 1 || n == -1) return sum2;    //result for 1
-    if (n > 0) {              //for positive numbers
-        for ( i = 2; i <= n; i++) {
-            result = sum1 + sum2;
-            sum1 = sum2;
-            sum2 = result;
-        }
-    }else{                //for negative numbers
-        for(i = -2; i >= n; i--){
-            result = sum1 - sum2;
-            sum1 = sum2;
-            sum2 = result;
-        }
-    }
-    return result;
-    
-var n =+ prompt('Input the number','');
-alert('Fibonacci munber for ' + n + ' is ' + fib(n));
+function Fibonacci(){
+    var a = 0;
+    var b = 1;
+    var f = 1;
+    for(i = 0; i <= 10; i++) {
+        f = a + b;
+        a = b;
+        b = f;
+        return f;
+        document.write(""+f+"<br>"); 
+     };
+}
