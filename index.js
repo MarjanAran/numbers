61961b6c499ec3cc1558e53051e0728b43c7246f
MarjanAran <whc.rion@gmail.com>
2016-10-06 17:09:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-function Fibonacci(){
+function Fibonacci(n){
     var a = 0;
     var b = 1;
-    var f = 1;
-    for(i = 0; i <= 10; i++) {
-        f = a + b;
+    for(var i = 0; i <= n; i++) {
+        var f = a + b;
         a = b;
         b = f;
-        return f;
-        document.write(""+f+"<br>"); 
+        return b;
      };
 }
+
+
+    document.write ( 'f1=' + Fibonacci(1) + '<br>');
+
+    document.write ( 'f3=' + Fibonacci(3) + '<br>');
+
+    document.write ( 'f5=' + Fibonacci(5) + '<br>');
+
+    document.write ( 'f7=' + Fibonacci(7) + '<br>');
